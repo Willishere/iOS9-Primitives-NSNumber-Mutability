a35d82fe0793a4c670323a688dd5d8d9e33ba996
William <chen.will7@gmail.com>
2019-11-07 10:20:23 -0800

    pointers!!!

--- iOs9Pointers/main.m
+++ iOs9Pointers/main.m
@@ -10,8 +10,43 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        printf("Hello  iOS9!\n");
+        
+        // Primitives
+        // C programming language
+        // C data types
+        
+        float piFloat = 3.14159265359;
+        double piDouble = 3.14159265359; //2x bytes compared to float (can store much bigger)
+        
+        //32-bit (4 bytes) to 64-bit (8 bytes)
+        //1 byte = 8 bits
+        // 0 0 0 0 0 0 0 0 // On/Off Yes/No true/false
+        // 1 2 3 4 5 6 7 8 // Index
+        // Binary = 1's and 0's
+        
+        printf("pifloat: %f\n", piFloat);
+        printf("piDouble: %.8f\n", piDouble);
+        
+        
+
+        long value = 42;
+        
+        printf("value: %li\n", value);
+        
+        // Address of Operator (&value)
+        long *addressOfValue = &value;
+        printf("addressOfValue: %p\n", addressOfValue);
+        printf("&addressOfValue: %p\n", addressOfValue);
+        
+        /// Pointrs (dereference operator =  *value)
+        printf("*addressOfValue: %li\n", *addressOfValue);
+        printf("value: %li\n", value);
+        
+        *addressOfValue = 75;
+        printf("CHANGED value: %li\n", value);
+        
     }
     return 0;
 }
